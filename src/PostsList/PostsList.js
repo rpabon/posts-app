40439be515d10bfafe7a132405a01179cdac81f7
Ricardo <rpabon257@gmail.com>
2018-10-07 20:01:26 +0200

    format file

--- src/PostsList/PostsList.js
+++ src/PostsList/PostsList.js
@@ -1,34 +1,33 @@
 import React, { PureComponent } from 'react';
 import axios from 'axios';
-import styles from './PostsList.css';
 
 export class PostsList extends PureComponent {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            posts: []
-        };
-    }
+    this.state = {
+      posts: []
+    };
+  }
 
-    async getPosts() {
-        const { data: posts } = await axios.get(
-            'https://jsonplaceholder.typicode.com/posts'
-        );
-        console.log(posts[0]);
-        this.setState({ posts });
-    }
-    componentDidMount() {
-        this.getPosts();
-    }
+  async getPosts() {
+    const { data: posts } = await axios.get(
+      'https://jsonplaceholder.typicode.com/posts'
+    );
 
-    render() {
-        return this.state.posts.map(({ id, title, body }) => (
-            <div key={id}>
-                <h3>{title}</h3>
-                <p>{body}</p>
-                <hr />
-            </div>
-        ));
-    }
+    this.setState({ posts });
+  }
+  componentDidMount() {
+    this.getPosts();
+  }
+
+  render() {
+    return this.state.posts.map(({ id, title, body }) => (
+      <div key={id}>
+        <h3>{title}</h3>
+        <p>{body}</p>
+        <hr />
+      </div>
+    ));
+  }
 }
