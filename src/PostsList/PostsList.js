44c21461dee3cf31dd7e37606a9d94e841a4a94c
Ricardo <rpabon257@gmail.com>
2018-10-07 19:58:05 +0200

    add .editorconfig

--- src/PostsList/PostsList.js
+++ src/PostsList/PostsList.js
@@ -3,32 +3,32 @@ import axios from 'axios';
 import styles from './PostsList.css';
 
 export class PostsList extends PureComponent {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      posts: []
-    };
-  }
+        this.state = {
+            posts: []
+        };
+    }
 
-  async getPosts() {
-    const { data: posts } = await axios.get(
-      'https://jsonplaceholder.typicode.com/posts'
-    );
-    console.log(posts[0]);
-    this.setState({ posts });
-  }
-  componentDidMount() {
-    this.getPosts();
-  }
+    async getPosts() {
+        const { data: posts } = await axios.get(
+            'https://jsonplaceholder.typicode.com/posts'
+        );
+        console.log(posts[0]);
+        this.setState({ posts });
+    }
+    componentDidMount() {
+        this.getPosts();
+    }
 
-  render() {
-    return this.state.posts.map(({ id, title, body }) => (
-      <div key={id}>
-        <h3>{title}</h3>
-        <p>{body}</p>
-        <hr />
-      </div>
-    ));
-  }
+    render() {
+        return this.state.posts.map(({ id, title, body }) => (
+            <div key={id}>
+                <h3>{title}</h3>
+                <p>{body}</p>
+                <hr />
+            </div>
+        ));
+    }
 }
